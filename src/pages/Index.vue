33d59dd5b897c1ca4184422336d0643e65e98918
Rafael Batista <rafael@devbatista.com>
2021-07-19 09:51:08 -0300

    Adicionando projeto front ao repositório

---
+++ src/pages/Index.vue
@@ -0,0 +1,73 @@
+<template>
+  <q-page class="container">
+    <div class="row q-mt-md">
+      <div class="col">
+        <q-btn color="primary">Criar novo</q-btn>
+      </div>
+    </div>
+    <div class="row q-mt-md">
+      <div class="col">
+        <q-table
+          class="q-mt-md"
+          title="Clientes"
+          no-data-label="Sem registros"
+          :columns="columns"
+        />
+      </div>
+    </div>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+const columns = [
+  {
+    name: 'cliente',
+    label: 'cliente',
+    align: 'left',
+    field: 'cliente'
+  },
+  {
+    name: 'email',
+    label: 'email',
+    align: 'left',
+    field: 'email'
+  },
+  {
+    name: 'ações',
+    label: 'ações',
+    align: 'left',
+    field: 'acoes'
+  }
+]
+
+export default defineComponent({
+  name: 'Data',
+  data () {
+    return {
+      data: []
+    }
+  },
+  mounted () {
+    this.getData()
+  },
+  methods: {
+    getData () {
+      this.$axios.get('http://localhost/api/clientes')
+        .then((res) => {
+          this.data = res.data
+          console.log('POSTS', res.data)
+        })
+        .catch((err) => {
+          console.error(err)
+        })
+    }
+  },
+  setup () {
+    return {
+      columns
+    }
+  }
+})
+</script>
---
+++ src/pages/create/Create.vue
@@ -0,0 +1,40 @@
+<template>
+    <q-page class="container">
+        <div class="row q-pa-md">
+            <h4>Adicionar novo cliente</h4>
+        </div>
+
+        <div class="q-pa-md">
+            <q-form action="http://localhost/api/cliente" method="post" class="q-gutter-md">
+                <q-input filled outlined v-model="name" label="Nome" type="text" name="nome" />
+                <q-input filled outlined v-model="email" label="Email" type="email" name="email" />
+                <q-input filled outlined v-model="descricao" label="Descrição" type="textarea" name="descricao" />
+                <q-input filled outlined v-model="tags" label="Palavras-chave" type="text" name="tags" />
+
+                <div class="row">
+                    <div class="col-6">
+                        <q-btn color="primary" class="pull-left" type="submit" :loading="submitting">Enviar</q-btn>
+                    </div>
+                    <div class="col-6">
+                        <q-btn color="black" class="float-right">Voltar</q-btn>
+                    </div>
+                </div>
+            </q-form>
+        </div>
+    </q-page>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  setup () {
+    return {
+      name: ref(''),
+      email: ref(''),
+      descricao: ref(''),
+      tags: ref('')
+    }
+  }
+}
+</script>
